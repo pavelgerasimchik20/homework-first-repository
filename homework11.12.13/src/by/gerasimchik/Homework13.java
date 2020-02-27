a1b72b589da92f61b74861bdd302a8822369715c
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-02-27 20:39:25 +0300

    homework8

---
+++ homework11.12.13/src/by/gerasimchik/Homework13.java
@@ -0,0 +1,30 @@
+package by.gerasimchik;
+
+import java.util.Scanner;
+
+public class Homework13 {
+    public static void main(String[] args) {
+        //ЗАДАНИЕ 13
+        while (true) {
+            System.out.println("Введите число для проверки : ");
+            Scanner scanner = new Scanner(System.in);
+            int a = scanner.nextInt();
+            if (a==0){
+                System.out.println("Не вводи ноль, плохой человек !!!");
+                continue;
+            }
+            boolean sost = false;
+            for (int i = 2; i < a; i++) {
+                if (a % i == 0) {
+                    sost = true;
+                    break;
+                }
+            }
+            if (sost == true) {
+                System.out.println("Введенное число - составное");
+            } else {
+                System.out.println("Введенное число - простое");
+            }
+        }
+    }
+}
