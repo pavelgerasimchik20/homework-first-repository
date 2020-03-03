3affa3e5f45f1ca7fc7eb954ef05bea096c85e5a
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-03-03 19:44:58 +0300

    homework 12 close

--- homework11.12.13/src/by/gerasimchik/Homework12.java
+++ homework11.12.13/src/by/gerasimchik/Homework12.java
@@ -3,12 +3,6 @@ package by.gerasimchik;
 public class Homework12 {
 
     public static void main(String[] args) {
-        int val = 123_383_999;
-        int sum = 0;
-        for (int i = 1000000000; i > ; i/1000) {
-            sum = val / i;
-            System.out.print(" " + sum);
-            sum=val;
-        }
+
     }
 }
