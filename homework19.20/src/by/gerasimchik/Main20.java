16ea72577f427429322593ebac82c07a122ccef3
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-03-03 23:17:17 +0300

    homework 20 (by swing)

--- homework19.20/src/by/gerasimchik/Main20.java
+++ homework19.20/src/by/gerasimchik/Main20.java
@@ -1,9 +1,20 @@
 package by.gerasimchik;
 
+import javax.swing.*;
+
 public class Main20 {
 
     public static void main(String[] args) {
 
+        String text = JOptionPane.showInputDialog("Введите текст:");
+        StringBuilder temporaryText = new StringBuilder();
 
+        text = text.replaceAll("[!.,:;-]", "");
+        String[] words = text.split(" ");
+
+        for(String word : words) {
+            temporaryText.append(word.charAt(word.length()-1));
+        }
+        JOptionPane.showMessageDialog(null,"Текст из последних букв в ваших словах:\n"+temporaryText);
     }
 }
