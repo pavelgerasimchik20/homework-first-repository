23842a6a16a55ed4f0c4ce6d18d0cfa4bd369935
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-03-06 14:41:11 +0300

    homework 20

--- homework19.20/src/by/gerasimchik/Main20.java
+++ homework19.20/src/by/gerasimchik/Main20.java
@@ -10,11 +10,12 @@ public class Main20 {
         StringBuilder temporaryText = new StringBuilder();
 
         text = text.replaceAll("[!.,:;-]", "");
+        text = text.replaceAll("-?[0-9]+", "");
         String[] words = text.split(" ");
 
-        for(String word : words) {
-            temporaryText.append(word.charAt(word.length()-1));
+        for (String word : words) {
+            temporaryText.append(word.charAt(word.length() - 1));
         }
-        JOptionPane.showMessageDialog(null,"Текст из последних букв в ваших словах:\n"+temporaryText);
+        JOptionPane.showMessageDialog(null, "Текст из последних букв в ваших словах:\n" + temporaryText);
     }
 }
