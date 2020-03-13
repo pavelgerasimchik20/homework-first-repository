c68ce920621228189136e58b89ba0d22d357bd5a
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-03-13 18:32:23 +0300

    Homework 15, 16

--- homework15.16/src/by/gerasimchik/Main.java
+++ homework15.16/src/by/gerasimchik/Main.java
@@ -3,14 +3,7 @@ package by.gerasimchik;
 public class Main {
 
     public static void main(String[] args) {
-        //ЗАДАНИЕ 15
-
-        int sum = 0;
-        int[] mass = {18, 0, 1, 2, 18, 2, 1, 0, 1, 10};
-        for (int i = 1; i < mass.length - 1; i++) {
-            sum += mass[i];
-        }
-        System.out.println("Сумма элементов массива (за исключением крайних)= " + sum);
+        //ЗАДАНИЕ 15 НАХОДИТСЯ В ДРУГОМ КОММИТЕ
 
         //Задание 16
         int[] fff = new int[15];
