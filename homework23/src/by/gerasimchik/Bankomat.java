4ae22408c21865c9fa5cb473417ef5ca81ea08e2
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-03-15 13:09:54 +0300

    homework 23

--- homework23/src/by/gerasimchik/Bankomat.java
+++ homework23/src/by/gerasimchik/Bankomat.java
@@ -6,6 +6,7 @@ public class Bankomat {
     private int fiftyDollar;
     private int hundredDollar;
 
+
     public Bankomat(int twentyDollar, int fiftyDollar, int hundredDollar) {
         this.twentyDollar = twentyDollar;
         this.fiftyDollar = fiftyDollar;
@@ -13,28 +14,69 @@ public class Bankomat {
     }
 
     public void addCash(int banknote20, int banknote50, int banknote100) {
+        System.out.println("Операция внесение денег в банкомат $$$ -------->>> ");
         twentyDollar += banknote20;
         fiftyDollar += banknote50;
         hundredDollar += banknote100;
+
     }
 
     public void showBanknote() {
         System.out.println("Остаток в банкомате : \n20$ : " + twentyDollar + "\n50$ : " + fiftyDollar + "\n100$ : " + hundredDollar);
     }
 
-    public boolean takeMoney(int Cash) {
-        if (Cash % 20 != 0) {
-            System.out.println("НЕКОРРЕКТНАЯ СУММА!!! Минимальная купюра 20$");
+    public boolean takeMoney(int cash) {
+        int sum = twentyDollar * 20 + fiftyDollar * 50 + hundredDollar * 100;
+        if (cash > sum) {
+            System.out.println("В банкомате НЕДОСТАТОЧНО средств. Макс. СУММА = " + sum);
+            return false;
+        } else if (cash == sum) {
+            int tw = twentyDollar;
+            twentyDollar -= twentyDollar;
+            int fi = fiftyDollar;
+            fiftyDollar -= fiftyDollar;
+            int hu = hundredDollar;
+            hundredDollar -= hundredDollar;
+            System.out.println("Выданы купюры: \n 20$ : " + tw + "\n50$ : " + fi + "\n100$ : " + hu);
+            return true;
+        } else if (cash % 10 != 0) {
+            System.out.println("Некорректная сумма. Мин.купюра 20$");
             return false;
         } else {
-            int a = (Cash - (Cash % 100)) / (100);
-            hundredDollar -= a;
-            fiftyDollar -= 0;
-            Cash = Cash % 100;
-            int b = Cash / 20;
-            twentyDollar -= b;
-            System.out.println("Выданы купюры: \n 20$ : " + b + "\n50$ : " + 0 + "\n100$ : " + a);
-            return true;
+            int hu = cash / 100;     //полное количество соток
+            if (hu >= hundredDollar ) {
+                int hun = hundredDollar;
+                hundredDollar -= hun-1;  //выдача всех соток
+                cash -= (hun-1) * 100;
+                int fi = cash / 50; //полное количество полтосиков
+                if (fi > fiftyDollar) {
+                    int fif = fiftyDollar;
+                    fiftyDollar -= fif; //выдача всех 50-ок
+                    cash -= fif * 50;
+                    int tw = cash / 20;
+                    twentyDollar -= tw;   //выдача 20-ок
+                    System.out.println("Выданы купюры: \n 20$ : " + tw + "\n50$ : " + fif + "\n100$ : " + hun);
+                } else {
+                    fiftyDollar -= fi;
+                    cash -= fi * 50;
+                    int tw = cash / 20;
+                    twentyDollar -= tw;
+                    System.out.println("Выданы купюры: \n 20$ : " + tw + "\n50$ : " + fi + "\n100$ : " + hu);
+                }
+
+            } else {
+                hundredDollar -= hu;
+                cash -= hu * 100;
+                int fi = cash / 50;
+                fiftyDollar -= fi;
+                cash -= fi * 50;
+                int tw = cash / 20;
+                twentyDollar -= tw;
+                System.out.println("Выданы купюры: \n 20$ : " + tw + "\n50$ : " + fi + "\n100$ : " + hu);
+            }
         }
+        return true;
     }
 }
+
+
--- homework23/src/by/gerasimchik/Main.java
+++ homework23/src/by/gerasimchik/Main.java
@@ -1,15 +1,24 @@
 package by.gerasimchik;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        Bankomat belinvestbank = new Bankomat(1000,500,200);
-
-        belinvestbank.addCash(100,100,100);
-
+        Bankomat belinvestbank = new Bankomat(100, 100, 100);
+        Scanner scanner = new Scanner(System.in);
         belinvestbank.showBanknote();
-        belinvestbank.takeMoney(1220);
-        belinvestbank.showBanknote();
+        //belinvestbank.addCash(10,10,10);
+
+        while (true) {
+            System.out.println("Запросите сумму в $ :");
+            int s = scanner.nextInt();
+            if (belinvestbank.takeMoney(s)) {
+                System.out.println("Операция прошла успешно");
+            } else {
+                System.out.println("Операция не выполнена");
+            }
+            belinvestbank.showBanknote();
+        }
     }
 }
