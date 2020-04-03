62e9cf628ba497193845d3da32602e4687ef2ede
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2020-04-03 17:26:41 +0300

    Homework 16 ( FIXED !!! )

--- homework15.16/src/by/gerasimchik/Main.java
+++ homework15.16/src/by/gerasimchik/Main.java
@@ -1,5 +1,7 @@
 package by.gerasimchik;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,11 +11,14 @@ public class Main {
         int[] fff = new int[15];
         for (int j = 0; j < fff.length; j++) {
             fff[j] = (int) (Math.random() * 10);
-            System.out.print("|" + fff[j] + "| ");
         }
+        System.out.print(Arrays.toString(fff));
         System.out.println();
-        for (int k = fff.length - 1; k >= 0; k--){
-            System.out.print("|" + fff[k] + "| ");
+        for (int i = 0; i < fff.length / 2; i++) {
+            int tmp = fff[i];
+            fff[i] = fff[fff.length - i - 1];
+            fff[fff.length - i - 1] = tmp;
         }
+        System.out.print(Arrays.toString(fff));
     }
 }
